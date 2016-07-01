569f7f99488b23908cd48d533a32c09d77c31f6b
Ian Lenehan <ianlenehan@gmail.com>
2016-07-01 10:07:37 +1000

    additional test, student redirect

--- spec/controllers/marks_controller_spec.rb
+++ spec/controllers/marks_controller_spec.rb
@@ -5,20 +5,20 @@ RSpec.describe MarksController, type: :controller do
   describe 'POST to /marks' do
     describe 'a mark with valid information' do
       before do
-        student = Student.create
+        @student = Student.create
         Mark.destroy_all
         mark = {
           :mark => 85,
-          :student_id => student.id,
+          :student_id => @student.id,
           :lesson_id => 1,
           :part => 1
         }
-        post :create, :mark => mark
+        post :create, :mark => mark, :id => @student.id
       end
 
-      # it 'should redirect to the student controller' do
-      #   expect(response).to redirect_to(student_update_path(:id))
-      # end
+      it 'should redirect to the student controller' do
+        expect(response).to redirect_to(student_update_path(@student.id))
+      end
 
       it 'should create a new mark' do
         expect(Mark.all.length).to eq(1)
